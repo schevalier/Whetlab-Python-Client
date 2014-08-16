3e784b46bc1cf82ca79475ca9d4bd72ab3aa88ad
Jasper Snoek <jaspersnoek@gmail.com>
2014-08-16 12:01:18 -0400

    whitespace...

--- whetlab/__init__.py
+++ whetlab/__init__.py
@@ -444,7 +444,7 @@ class Experiment:
                 res = self._client.experiments().create(name=self.experiment, 
                                                         description=self.experiment_description,
                                                         settings=settings)
-                self.experiment_id = res.body['id']
+                self.experiment_id = res.body['id']                
             except Exception as inst:
                 # If experiment creation doesn't work, then retry resuming the experiment.
                 # This is for cases where two processes are starting an experiment, and
